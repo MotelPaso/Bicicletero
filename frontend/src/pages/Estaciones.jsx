0f2f49721d5845f7f1f8dbf4d619883e3c73cd3c
MotelPaso <pauloaraya42@gmail.com>
2025-11-18 12:55:43 -0300

    feat: frontend landing page
    finished frontend landing page and added a bg pic

--- frontend/src/pages/Estaciones.jsx
+++ frontend/src/pages/Estaciones.jsx
@@ -1,5 +1,6 @@
 import Estacion from "../components/Estacion";
 import api from "../services/api";
+import "../global.css";
 import { useEffect, useState } from "react";
 
 function Estaciones() {
@@ -30,10 +31,17 @@ function Estaciones() {
 
 	return (
 		<div className="flex flex-col justify-around">
+			<div className="flex justify-center">
+				<h1 className="font-bold text-[48px] text-(--DTitle)">
+					Lista de Estaciones
+				</h1>
+			</div>
 			{error && (
 				<>
 					<div className="flex flex-row justify-center items-center gap-4">
-						<h2>No he podido cargar las estaciones...</h2>
+						<h2 className="font-normal text-[24px] text-(--DText)">
+							No he podido cargar las estaciones...
+						</h2>
 						<button
 							onClick={recargarPagina}
 							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -43,7 +51,6 @@ function Estaciones() {
 					</div>
 				</>
 			)}
-			<h1>Estaciones:</h1>
 			{estaciones.map((estacion) => (
 				<Estacion datos={estacion} key={estacion.id} />
 			))}
--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 
 function Inicio() {
 	return (
-		<div className="flex pt-8">
-			<div className="flex flex-col w-[60%]">
-				<h1 className="text-(--DTitle) font-bold text-[64px] text-left">
-					¡Únete al cambio!
-				</h1>
-				<h6 className="text-(--DText) font-normal text-[24px] text-left">
-					Sé parte de los cientos de personas usan bicicletas para
-					moverse por la ciudad.
-				</h6>
-			</div>
-			<div className="flex justify-end items-center w-[30%]">
-				<Link to="/registro">
-					<button className="bg-black text-white rounded-lg w-52 h-24 text-[24px]">
-						Registrarme
-					</button>
-				</Link>
+		<div className="relative w-full">
+			<img
+				src="/fotoBicis.jpg"
+				className="w-full h-[620px] object-cover"
+			/>
+
+			<div className="absolute top-0 left-0 w-full h-full flex flex-row justify-start pt-16 px-8">
+				<div className="text-left w-[80%]">
+					<h1 className="font-bold text-[64px] text-(--DBG) drop-shadow-xl">
+						¡Únete al cambio!
+					</h1>
+					<h6 className="font-semibold text-[24px] text-(--DBG) drop-shadow">
+						Sé parte de los cientos de personas usan bicicletas para
+						moverse por la ciudad.
+					</h6>
+				</div>
+
+				<div className="mt-8">
+					<Link to="/registro">
+						<button className="bg-black text-white rounded-lg w-52 h-20 text-[24px]">
+							Registrarme
+						</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
