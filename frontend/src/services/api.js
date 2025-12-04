c7849b9d322ab10073db25aa395eb11103fe8389
Paulo Araya <paulo.araya02@alumnos.ucn.cl>
2025-12-04 13:43:01 -0300

    new ip change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-let API_URL = import.meta.env.VITE_API_LOCALPROD;
-
-if (!API_URL) {
-	console.error("URL no definida, usaremos la local...");
-	API_URL = "http://127.0.0.1:8000";
-}
+let API_URL = "http://179.8.199.30:8000";
 
 const api = axios.create({
 	baseURL: API_URL,
