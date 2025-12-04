56996cc029ab4e7c2c62c58572879f185c16a3bf
Paulo Araya <paulo.araya02@alumnos.ucn.cl>
2025-12-04 13:30:13 -0300

    change local ip to prod

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+let API_URL = import.meta.env.VITE_API_LOCALPROD;
+
+if (!API_URL) {
+	console.error("URL no definida, usaremos la local...");
+	API_URL = "http://127.0.0.1:8000";
+}
+
 const api = axios.create({
-	baseURL: "http://192.168.1.20:8000",
+	baseURL: API_URL,
 });
 
 export default api;
